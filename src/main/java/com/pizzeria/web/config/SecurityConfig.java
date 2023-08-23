ec9a8c04c9960fcf28eaced6ed12cb48522e82c6
Imer Coaguila <imertpro@gmail.com>
2023-08-23 15:32:46 -0400

    users with role added

--- src/main/java/com/pizzeria/web/config/SecurityConfig.java
+++ src/main/java/com/pizzeria/web/config/SecurityConfig.java
@@ -6,6 +6,12 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,12 +23,32 @@ public class SecurityConfig {
                     customizeRequests
                             //aca ya no ponemos /api/ porque esa es la raiz, se pone directamente la ruta del controlador
                             //con un * solo permitimos el primer nivel con ** permitimos todo para adelante de la ruta
-                            .requestMatchers(HttpMethod.GET, "/*").permitAll()
-                            .requestMatchers(HttpMethod.PUT).denyAll()
+                            .requestMatchers(HttpMethod.GET, "/pizzas/**").hasAnyRole("ADMIN", "CUSTOMER")
+                            .requestMatchers(HttpMethod.POST, "/pizzas/**").hasRole("ADMIN")
+                            .requestMatchers(HttpMethod.PUT).hasRole("ADMIN")
+                            .requestMatchers(HttpMethod.GET, "/pizzaOrders/**").hasRole("ADMIN")
                             .anyRequest().authenticated();
                 }).csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
                 .cors(Customizer.withDefaults());
         return httpSecurity.build();
+    }
+    @Bean
+    public UserDetailsService memoryUsers(){
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+        UserDetails customer = User.builder()
+                .username("customer")
+                .password(passwordEncoder().encode("customer123"))
+                .roles("CUSTOMER")
+                .build();
+        return new InMemoryUserDetailsManager(admin, customer);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
