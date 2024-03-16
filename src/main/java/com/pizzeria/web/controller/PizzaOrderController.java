7a7b7e9d323f02d6373e3c1555aa46d954a93356
imertetsu <imertpro@gmail.com>
2024-03-16 12:15:28 -0400

    Tags and Operations added to pizza order controller

--- src/main/java/com/pizzeria/web/controller/PizzaOrderController.java
+++ src/main/java/com/pizzeria/web/controller/PizzaOrderController.java
@@ -5,12 +5,15 @@ import com.pizzeria.persistence.entity.PizzaOrderEntity;
 import com.pizzeria.persistence.projection.OrderSummary;
 import com.pizzeria.service.PizzaOrderService;
 import com.pizzeria.service.dto.RandomOrderDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Pizza Order Controller", description = "In this section we are going to create, read, update and delete Pizza Orders")
 @RestController
 @RequestMapping("/pizzaOrders")
 public class PizzaOrderController {
@@ -20,29 +23,35 @@ public class PizzaOrderController {
         this.pizzaOrderService = pizzaOrderService;
     }
 
+    @Operation(summary = "Get all pizza orders", description = "We are going to retrieve a list of all pizza orders")
     @GetMapping()
     public ResponseEntity<List<PizzaOrderEntity>> getOrders(){
         return ResponseEntity.ok(this.pizzaOrderService.getAllOrders());
     }
+    @Operation(summary = "Get all pizza orders by date", description = "We are going to retrieve a list of all pizza orders by its date")
     @GetMapping("/date")
     public ResponseEntity<List<PizzaOrderEntity>> getOrdersByDate(){
         return ResponseEntity.ok(this.pizzaOrderService.getOrdersAfter());
     }
+    @Operation(summary = "Get all pizza orders outside", description = "We are going to retrieve a list of all pizza orders outside")
     @GetMapping("/outside")
     public ResponseEntity<List<PizzaOrderEntity>> getOrdersByMethodsOutside(){
         return ResponseEntity.ok(this.pizzaOrderService.getOrdersByMethodOutside());
     }
 
+    @Operation(summary = "Get all pizza orders by a customer", description = "We are going to retrieve a list of all pizza orders by a customer id")
     @GetMapping("/customer/{customerId}")
     public ResponseEntity<List<PizzaOrderEntity>> getCustomerOrders(@PathVariable("customerId") String customerId){
         return ResponseEntity.ok(this.pizzaOrderService.getCustomerOrders(customerId));
     }
 
+    @Operation(summary = "Get pizza order by id", description = "We are going to retrieve a pizza order by its id")
     @GetMapping("/customizedOrder/{orderId}")
     public ResponseEntity<OrderSummary> getSummaryOrder(@PathVariable("orderId") int orderId){
         return ResponseEntity.ok(this.pizzaOrderService.getSummaryOrder(orderId));
     }
 
+    @Operation(summary = "Save a random pizza order", description = "We are going to save a random pizza order with 20% OFF")
     @PostMapping("/random")
     public ResponseEntity<Boolean> randomPizzaOrder(@RequestBody RandomOrderDTO randomOrderDTO){
         return ResponseEntity.ok(this.pizzaOrderService.saveRandomOrder(randomOrderDTO));
