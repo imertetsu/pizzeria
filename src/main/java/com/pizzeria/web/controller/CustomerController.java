67a426c103e497cb745a45e679b4efaaea6a2be5
imertetsu <imertpro@gmail.com>
2024-03-16 11:59:02 -0400

    Tags and Operations added to customer and pizza controllers

--- src/main/java/com/pizzeria/web/controller/CustomerController.java
+++ src/main/java/com/pizzeria/web/controller/CustomerController.java
@@ -4,6 +4,7 @@ import com.pizzeria.persistence.entity.CustomerEntity;
 import com.pizzeria.persistence.entity.PizzaOrderEntity;
 import com.pizzeria.service.CustomerService;
 import com.pizzeria.service.PizzaOrderService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Tag(name = "Customer CRUD controller", description = "In this section we are going to create, read, update and delete a customer")
+@Tag(name = "Customer controller", description = "In this section we are going to create, read, update and delete Customers")
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
@@ -28,15 +29,17 @@ public class CustomerController {
         this.customerService = customerService;
         this.pizzaOrderService = pizzaOrderService;
     }
+    @Operation(summary = "Get all customers", description = "We are going to retrieve a list of all customers")
     @GetMapping()
     public ResponseEntity<List<CustomerEntity>> getAllCustomers(){
         return ResponseEntity.ok(this.customerService.getAllCustomers());
     }
+    @Operation(summary = "Get a customer by phone number", description = "We are going to retrieve a customer by his/her phone number")
     @GetMapping("/phone/{phone}")
     public ResponseEntity<CustomerEntity> getCustomerByPhone(@PathVariable("phone") String phone){
         return ResponseEntity.ok(this.customerService.getCustomerByPhone(phone));
     }
-
+    @Operation(summary = "Get a customer by id", description = "We are going to retrieve a customer by his/her id")
     @GetMapping("/customer/{customerId}")
     public ResponseEntity<List<PizzaOrderEntity>> getCustomerOrders(@PathVariable("customerId") String customerId){
         return ResponseEntity.ok(this.pizzaOrderService.getCustomerOrders(customerId));
